02f1a25467fe608300dd3afe675ef2e3f60ef681
Shane Lester <shanejlester@gmail.com>
2019-11-27 20:37:48 +0000

    Make no line much over 80 characters for cleaner code

--- backend/network_simulator/business_logic/grid.py
+++ backend/network_simulator/business_logic/grid.py
@@ -17,11 +17,14 @@ class Grid:
         """
         Will Generate a grid of size 10 * 10 as an array of arrays
         """
-        # TODO: Add decorator for input validation: 1 to 9 base stations and base_stations to 20?? routable devices
-        # TODO: Make sure that the global_id never reaches near 100. Make sure to test on tons of values to see what
+        # TODO: Add decorator for input validation: 1 to 9 base stations and
+        # base_stations to 20?? routable devices
+        # TODO: Make sure that the global_id never reaches near 100. Make sure
+        # to test on tons of values to see what
         # works consistently to define input bounds
         self.global_id_inc = 1
-        self.grid = [[self.EMPTY_SPACE for _ in range(self.DIMENSIONS)] for _ in range(self.DIMENSIONS)]
+        self.grid = [[self.EMPTY_SPACE for _ in range(self.DIMENSIONS)] 
+                      for _ in range(self.DIMENSIONS)]
         self._add_devices(num_base_stations, num_devices)
         
     def __repr__(self):
@@ -32,18 +35,25 @@ class Grid:
         
     def _scan_area_around_base_station_for_placement(self, x_coor, y_coor):
         """
-        Scans the transmission self.TRANSMISSION_RADIUS make sure that the base stations are far
-        enough apart.
+        Scans the transmission self.TRANSMISSION_RADIUS make sure that the
+        base stations are far enough apart.
         """
         # Board bounds can't be below 0
         check_and_fix_lower_bounds = lambda x: 0 if x < 0 else x 
         # Board bounds can't be below self.DIMENSIONS-1
-        check_and_fix_upper_bounds = lambda x: self.DIMENSIONS - 1 if x > self.DIMENSIONS - 1 else x
+        check_and_fix_upper_bounds = (lambda x:
+                                      self.DIMENSIONS - 1
+                                      if x > self.DIMENSIONS - 1
+                                      else x)
         create_bounds = namedtuple("bounds", "x0 x1 y0 y1")
-        boundaries = create_bounds(check_and_fix_lower_bounds(y_coor - self.TRANSMISSION_RADIUS),
-                                   check_and_fix_upper_bounds(y_coor + self.TRANSMISSION_RADIUS),
-                                   check_and_fix_lower_bounds(x_coor - self.TRANSMISSION_RADIUS),
-                                   check_and_fix_upper_bounds(x_coor + self.TRANSMISSION_RADIUS))
+        boundaries = create_bounds(check_and_fix_lower_bounds(y_coor -
+                                    self.TRANSMISSION_RADIUS),
+                                   check_and_fix_upper_bounds(y_coor +
+                                    self.TRANSMISSION_RADIUS),
+                                   check_and_fix_lower_bounds(x_coor -
+                                    self.TRANSMISSION_RADIUS),
+                                   check_and_fix_upper_bounds(x_coor +
+                                    self.TRANSMISSION_RADIUS))
         for i in range(boundaries.x0, boundaries.x1 + 1):
             for j in range(boundaries.y0, boundaries.y1 + 1):
                 if self.grid[j][i][0] == self.BASE_STATION_ROOT:
@@ -56,32 +66,45 @@ class Grid:
         """
         # TODO: Refactor this line to work with all inputs
         devices_per_base_station = int(num_devices / num_base_stations)
+        # Add in base stations
         for _ in range(num_base_stations):
             while True:
                 x_coor = random.randint(1,10) - 1
                 y_coor = random.randint(1, 10) - 1
                 if self.grid[y_coor][x_coor] != self.EMPTY_SPACE:
                     continue
-                is_space_free, base_station_boundaries = self._scan_area_around_base_station_for_placement(y_coor, x_coor)
+                
+                is_space_free, base_station_boundaries = \
+                self._scan_area_around_base_station_for_placement(y_coor, x_coor)
+                
                 if is_space_free:
-                    self.grid[y_coor][x_coor] = (self.BASE_STATION_ROOT + 
-                                                 ("0" if 0 < self.global_id_inc < 10 else "") + 
-                                                 str(self.global_id_inc))
+                    self.grid[y_coor][x_coor] = \
+                        (self.BASE_STATION_ROOT + 
+                        ("0" if 0 < self.global_id_inc < 10 else "") + 
+                        str(self.global_id_inc))
                     self.global_id_inc += 1
+                    
+                    # For the transimssion radius of a base station,
+                    # add its associated user devices
                     for _ in range(devices_per_base_station):
                         while True:
                             x_coor = random.randint(1,10) - 1
                             y_coor = random.randint(1, 10) - 1
                             if (self.grid[y_coor][x_coor] == self.EMPTY_SPACE
-                                and base_station_boundaries.x0 <= x_coor <= base_station_boundaries.x1
-                                and base_station_boundaries.y0 <= y_coor <= base_station_boundaries.y1):
-                                    self.grid[y_coor][x_coor] = (self.ROUTABLE_DEVICE_ROOT + 
-                                                                ("0" if 0 < self.global_id_inc < 10 else "") + 
-                                                                str(self.global_id_inc))
+                                and base_station_boundaries.x0 
+                                    <= x_coor <= base_station_boundaries.x1
+                                and base_station_boundaries.y0 
+                                    <= y_coor <= base_station_boundaries.y1):
+                                    self.grid[y_coor][x_coor] = \
+                                    (self.ROUTABLE_DEVICE_ROOT + 
+                                    ("0" if 0 < self.global_id_inc < 10 else "") + 
+                                    str(self.global_id_inc))
                                     self.global_id_inc += 1
                                     break
                     break
             
 if __name__ == "__main__":
-    x = Grid(5, 25)
+    # TODO: As is we need to have the number of devices evenly divide the
+    # number of base stations
+    x = Grid(4, 12)
     print(x)
