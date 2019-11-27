1de26d12909207d562f30d355fbb1bd338daf62e
Shane Lester <shanejlester@gmail.com>
2019-11-27 15:56:42 +0000

    Add repr string that is the grid

--- backend/network_simulator/business_logic/grid.py
+++ backend/network_simulator/business_logic/grid.py
@@ -1,48 +1,65 @@
 from networkx import nx
 import math
 import random
+import base_station
 
 class Grid:
     
     DIMENSIONS = 10
     NUM_CHANNELS = 5
     
-    def __init__(self, num_base_stations, num_devices):
-        self.grid = [[None for _ in range(self.DIMENSIONS)] for _ in range(self.DIMENSIONS)]
-        self.baseStations = []
-        self.userDevices = []
-        self.coordinatesMap = {}
-        self.graph = nx.Graph()
-        #self.init_random(num_base_stations, num_devices)
+    def __init__(self):
+        """
+        Will Generate a grid of size 10 * 10 as an array of arrays
+        """
+        self.grid = [["_" for _ in range(self.DIMENSIONS)]for _ in range(self.DIMENSIONS)]
         
-    def show_grid(self):
-        def transform_list(arr):
-            output_arr = []
-            for _, elem in enumerate(arr):
-                if elem == None:
-                    output_arr.append(" -- ")
-                elif elem == ",":
-                    output_arr.append("")
-                elif elem == "[":
-                    output_arr.append("|")
-                elif elem == "]":
-                    output_arr.append("|")
-            print("".join(output_arr))
-            
+    def __repr__(self):
+        repr_str = ""
         for row in self.grid:
-            transform_list(row)
-                
+            repr_str = repr_str + " ".join(row) + "\n"
+        return repr_str
+    
+#    def __init__(self, num_base_stations, num_devices):
+#        self.grid = [[None for _ in range(self.DIMENSIONS)] for _ in range(self.DIMENSIONS)]
+#        self.base_stations = []
+#        self.user_devices = []
+#        self.coordinates_map = {}
+#        self.graph = nx.Graph()
+#        self.init_random(num_base_stations, num_devices)
+#        
+#    def show_grid(self):
+#        def transform_list(arr):
+#            output_arr = []
+#            for _, elem in enumerate(arr):
+#                if elem == None:
+#                    output_arr.append(" -- ")
+#                elif elem == ",":
+#                    output_arr.append("")
+#                elif elem == "[":
+#                    output_arr.append("|")
+#                elif elem == "]":
+#                    output_arr.append("|")
+#            print("".join(output_arr))
+#        
+#        for row in self.grid:
+#            transform_list(row)
+#            
+#            
+#    def select_channel(self):
+#        return random.randint(1, self.NUM_CHANNELS)
+#        
+#                
 
-if __name__ == "__main__":
-    x = Grid(3, 10)
-    x.show_grid()
-        
+#        
 #    def init_random(self, num_base_stations, num_devices):
 #        possibleCoordinates = []
 #        
-#        # Partition graph into regions by x coordinate
+#        #Partition graph into regions by x coordinate
 #        region_size = math.ceil(self.DIMENSIONS / num_base_stations)
+#        print(region_size)
 #        num_of_regions = math.ceil(self.DIMENSIONS / region_size)
+#        print(num_of_regions)
 #        
 #        for i in range(self.DIMENSIONS):
 #            for j in range(self.DIMENSIONS):
@@ -50,7 +67,7 @@ if __name__ == "__main__":
 #                
 #        max_index = self.DIMENSIONS ** 2
 #        
-#        #regions = [0 for _ in range(region_size)]
+#        regions = [0 for _ in range(region_size)]
 #        regions = []
 #        
 #        for i in range(num_of_regions):
@@ -67,8 +84,17 @@ if __name__ == "__main__":
 #        
 #        for i in range(num_base_stations):
 #            region = regions[i%num_of_regions]
-#            coordinate = region[0]
+#            coordinates = region[0]
 #            del region[0]
 #            base_station_coordinates.append(coordinates)
-#            base_station_id = "b"+i
-#            self.grid[coor]
+#            base_station_id = "b"+str(i)
+#            self.grid[coordinates[0]][coordinates[1]] = base_station.BaseStation(base_station_id)
+#            self.coordinates_map[base_station_id] = coordinates
+#            self.base_stations.append(base_station_id)
+#            
+#        
+#            
+            
+if __name__ == "__main__":
+    x = Grid()
+    print(x)
