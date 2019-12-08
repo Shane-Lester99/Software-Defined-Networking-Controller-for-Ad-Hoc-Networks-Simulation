1c546ee591aebbd7e5a11525396181d540682cf0
Shane Lester <shanejlester@gmail.com>
2019-12-08 01:36:52 +0000

    Add query for blocked channel logic

--- backend/network_simulator/application_logic/channel.py
+++ backend/network_simulator/application_logic/channel.py
@@ -1,17 +1,57 @@
 import numpy
+from decorators import validate_amount, validate_path
+from collections import namedtuple
 
+blocked_channel_entry = namedtuple("BlockedChannel", "chan_coor chan_used")
 
 class Channels:
-    def __init__(self, amount):
+    
+    @validate_amount
+    def __init__(self, amount, transmission_radius):
+        self.transmission_radius = transmission_radius
         self.channels = [numpy.random.exponential() for _ in range(amount)]
         
     def __repr__(self):
         chan_str = str(["Channel {}: {}".format(i, exp) for i, exp in enumerate(self.channels)])
         return "Channels({})".format(chan_str)
-
+    
+    # @validate_path   
+    # def enumerate_available_channels(self, global_interference, coor_path):
+    #     def find_paths():
+    #         if len(curr) == len(coor_path) - 1:
+    #             output.append(curr.copy())
+    #         for edge_num in len(coor_path-1):
+    #             pass
+    #     output = [] 
+    #     find_pathts(coor_path, curr, output)
+    #     return False
+    
+    def _check_blocked_channels(self, blocked_channels, curr_coor):
+        """
+        This checks if given coordinate if there all blocked channels that
+        are unavailable. It returns a set of unavailable channels give a coor
+        """
+        dont_use_channels = set()
+        for blocked_chan in blocked_channels:
+            curr_coor_x, curr_coor_y = curr_coor
+            block_coor_x, block_coor_y = blocked_chan.chan_coor
+            if (abs(curr_coor_x - block_coor_x) <= self.transmission_radius and
+                abs(curr_coor_y - block_coor_y) <= self.transmission_radius):
+                    # If we find a channel with interference we don't use the
+                    # adjaceny channels either
+                    dont_use_channels.add(blocked_chan.chan_used)
+                    dont_use_channels.add(blocked_chan.chan_used - 1)
+                    dont_use_channels.add(blocked_chan.chan_used + 1)
+        return dont_use_channels
+        
 if __name__ == "__main__":
-    x = Channels(5)
-    print(x)
+    sys_channels = Channels(5, 2)
+    x = [blocked_channel_entry((6,5), 0), blocked_channel_entry((6,6), 2), blocked_channel_entry((7,8), 4)]
+    print(sys_channels._check_blocked_channels(x, (6,8)))
+    # path = [(6,5,), (6,6,), (7,8,), (9,7,)]
+    # print(sys_channels)
+    # x = sys_channels.enumerate_available_channels(None, path)
+    # print(x)
 
 
 
