81b385ed39193b4216af6138cab3f419011f389d
Shane Lester <shanejlester@gmail.com>
2019-12-15 01:57:35 +0000

    Create stat manager module

--- backend/network_simulator/application_logic/stat_manager.py
+++ backend/network_simulator/application_logic/stat_manager.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 class StatManager:
     """
     Manages the statistics of the route simulator for generating performance
@@ -13,14 +16,54 @@ class StatManager:
     map is the variables that we hold constant.
     
     E.g. for (number_of_channels, channel_switches)
-    20_4: {5: [2, 3, 4], 6: [1,3,4, 6], 7: [6,4]}
+    20_4: {5: [2, 3, 4], 6: [1,3,4,6], 7: [6,4]}
     this means when holding number of nodes to 20 and hops to 4, we have
     generated the scatter plot of 5 on the x axis with values 2 3 and 4 on the
     y axis for 5 channels the amount of channel switches we had holding those
     other two variables constant. Same for 6 and 7.
     """
-    def ___init___(self):
-        pass
     
-    def create_key(self, node_number, channel_number):
-        return str(node_number) + "_" + str(channel_number)
+    CHAN_SWITCH = "chan_switch"
+    CHAN_HOP = "chan_hop"
+    NODE_SWITCH = "node_switch"
+    NODE_HOP = "node_hop"
+    
+    
+    def ___init___(self):
+        self.stat_manager = {
+            self.CHAN_SWITCH : defaultdict(list),
+            self.CHAN_HOP: defaultdict(list),
+            self.NODE_SWITCH: defaultdict(list),
+            self.NODE_HOP: defaultdict(list)
+        }
+        
+    def __repr__(self):
+        return "StatManager(Channels/Switches(...), Channels/Hops(...), Nodes/Switches(...), Nodes/Hops(...)"
+    
+    def collect_stats_from_route_data(self, num_channels, num_nodes,
+                                      num_hops, num_switches):
+        """
+        Will take data from route job and package it into statistics that can
+        be queried into charts
+        """
+        # NOTE: gen_key will generate a key of the variables we hold constant
+        gen_key = lambda x, y: str(x) + "_" + str(y)
+        # CHAN_SWITCH CHART
+        chan_switch_key = gen_key(num_nodes, num_hops)
+        self.stat_manager[self.CHAN_SWITCH][chan_switch_key][str(num_channels)].append(num_switches)
+        
+        # CHAN_HOP chart
+        chan_hop_key = gen_key(num_nodes, num_switches)
+        self.stat_manager[self.CHAN_HOP][chan_hop_key][str(num_channels)].append(num_hops)
+        
+        # NODE_SWITCH chart
+        node_switch_key = gen_key(num_channels, num_hops)
+        self.stat_manager[self.NODE_SWITCH][node_switch_key][str(num_nodes)].append(num_switches)
+        
+        # NODE_HOP chart
+        node_hop_key = gen_key(num_channels, num_switches)
+        self.stat_manager[self.NODE_HOP][node_switch_key][str(num_nodes)].append(num_hops)
+        
+if __name__ == "__main__":
+    x = StatManager()
+    print(x)
