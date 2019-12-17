9a7eaa9d8cb24babcfb503f2d560ad7e109869d5
Shane Lester <shanejlester@gmail.com>
2019-12-17 21:23:55 +0000

    Add in reset-able graph for collecting stats on many graphs

--- backend/network_simulator/application_logic/network_simulation_entry_point.py
+++ backend/network_simulator/application_logic/network_simulation_entry_point.py
@@ -10,14 +10,21 @@ def run_cli_in_main():
     debug the backend
     """
     print("This is the CLI version of the Network Routing Simulation.")
-    print("To set up the network topology, write a list of 1 to 8 numbers.")
-    print("Each number signifies how many user devices are associated with that base station.\n")
-    bs_str = input("Please enter these numbers now as a single space seperated list:")
-    bs_list = [int(char) for char in bs_str if char != " "]
-    chan_amount_str = input("Please enter an amount of channels between 4 and 10:")
-    chan_amount = int(chan_amount_str)
-    entry = NetworkSimulationEntryPoint(bs_list, chan_amount)
-    entry.run_cli()
+    entry = NetworkSimulationEntryPoint()
+    while True:
+        x = input("Would you like to build a random graph. (Y/N):")
+        if x == "y" or x == "Y":
+            print("\nTo set up the network topology, write a list of 1 to 8 numbers.")
+            print("Each number signifies how many user devices are associated with that base station.\n")
+            bs_str = input("Please enter these numbers now as a single space seperated list:")
+            bs_list = [int(char) for char in bs_str if char != " "]
+            chan_amount_str = input("Please enter an amount of channels between 4 and 10:")
+            chan_amount = int(chan_amount_str)
+            entry.run_cli_instance(bs_list, chan_amount)
+        else:
+            break
+        entry.reset_graph()
+    print("Goodbye.")
 
 class NetworkSimulationEntryPoint:
     """
@@ -29,41 +36,12 @@ class NetworkSimulationEntryPoint:
     To run the CLI version run the module from the main function
     """
     
-    def __init__(self, base_station_list, channel_amount):
-        # nodes on the x axis by number, and it will map to channel switches and
-        # number of hops of the y axis
-        # number of channels on the x axis with channel switches and hops on the
-        # y axis
+    def __init__(self):
         self._stat_manager = StatManager()
-        self._entry_grid = grid.Grid(base_station_list)
-        self._entry_graph = graph.RoutingSystemMasterGraph(
-            self._entry_grid.device_data,
-            self._entry_grid.TRANSMISSION_RADIUS,
-            channel_amount)
-            
-    def run_cli(self):
-        """
-        This will allow the user to run the app as a cli without the use of the API. 
-        THe user can write queries on the command line and once they exit they
-        will retrieve the system output stats. The purpose of this function is
-        for backend debugging.
-        """
-        print(self._entry_grid)
-        print(self._entry_graph.channels)
-        print(self.retrieve_random_graph_as_json())
-        while True:
-            exit_string = input("Would you like to specify a query path (Y/N):")
-            if exit_string == "N" or exit_string == "n":
-                print(self.retrieve_system_results_as_json())
-                break
-            x = input("Please specify query path in form <device_id_1><device_id2>:")
-            source, dest = x[:3], x[3:]
-            print(self.retrieve_query_results_as_json(source, dest))
-            
-    def retrieve_channels_as_json(self):
-        return json.dumps(self._entry_graph.channels.channels)
-            
-    def retrieve_random_graph_as_json(self):
+        self._entry_grid = None
+        self._entry_graph = None
+        
+    def retrieve_random_graph_as_json(self, bs_list= None, channel_amount= None):
         """
         For the initialization function call of the API. This function will return
         the random graph that will remain static after initialization.
@@ -85,6 +63,11 @@ class NetworkSimulationEntryPoint:
             }
         }
         """
+        if not self._entry_grid and not self._entry_graph:
+            self._entry_grid = grid.Grid(bs_list)
+            self._entry_graph = graph.RoutingSystemMasterGraph(self._entry_grid.device_data,
+                                                               self._entry_grid.TRANSMISSION_RADIUS,
+                                                           channel_amount)
         json_dict = dict()
         for node_name, entries in self._entry_graph.graph.items():
             metadata = entries[0]
@@ -99,6 +82,35 @@ class NetworkSimulationEntryPoint:
                  "edges": string_connected_edges
             }
         return json.dumps(json_dict)
+        
+    def reset_graph(self):
+        self._entry_grid = None
+        self._entry_graph = None
+            
+    def run_cli_instance(self, bs_list, channel_amount):
+        """
+        This will allow the user to run the app as a cli without the use of the API. 
+        THe user can write queries on the command line and once they exit they
+        will retrieve the system output stats. The purpose of this function is
+        for backend debugging.
+        """
+        self.retrieve_random_graph_as_json(bs_list, channel_amount)
+        print(self._entry_grid)
+        print(self._entry_graph.channels)
+        print(self.retrieve_random_graph_as_json())
+        while True:
+            exit_string = input("Would you like to specify a query path (Y/N):")
+            if exit_string == "N" or exit_string == "n":
+                print("\nSystem Stats:\n")
+                print("\t", self.retrieve_system_results_as_json(), end="\n\n")
+                break
+            x = input("Please specify query path in form <device_id_1><device_id2>:")
+            source, dest = x[:3], x[3:]
+            print(self.retrieve_query_results_as_json(source, dest))
+            
+    def retrieve_channels_as_json(self):
+        return json.dumps(self._entry_graph.channels.channels)
+            
     
     def retrieve_query_results_as_json(self, source_node, dest_node):
         """
@@ -110,7 +122,8 @@ class NetworkSimulationEntryPoint:
         number_of_channels = len(self._entry_graph.channels.channels)
         number_of_nodes = len(self._entry_graph)
         number_of_hops = len(route) - 1
-        number_of_switches = len(set([channel for channels_used in route.values() for channel in channels_used]))
+        number_of_switches = len(set([channel for channels_used in route.values()
+                                 for channel in channels_used]))
         if route:
             self._stat_manager.collect_stats_from_route_data(number_of_channels,
                                                              number_of_nodes,
@@ -122,83 +135,6 @@ class NetworkSimulationEntryPoint:
     def retrieve_system_results_as_json(self):
         return json.dumps(self._stat_manager.stats)
     
-    # def retrieve_system_results_as_json(self):
-    #     """
-    #     The API allows for seeing all the results of all the queries.
-    #     """
-    #     json_dict = dict()
-    #     for date_str, stat_pkg in self._entry_graph.sys_stats.items():
-    #         self._convert_one_query_stat_block_to_json(json_dict,
-    #                                                   (date_str, stat_pkg,)
-    #                                                   )
-    #     return json.dumps(json_dict)
-    
-    # def _convert_one_query_stat_block_to_json(self, json_dict, stat_pkg):
-    #     """
-    #     Converts statistics from query into a dictionary that can be converted
-    #     to json string by json.dumps. 
-    #     JSON is in form:
-    #     {
-    #         date_str: {
-    #             cost: <Float>,
-    #             path: <List>,
-    #             results: {
-    #                 R22_R02: {
-    #                     channel: {
-    #                         id: <int>
-    #                         weight: <float>
-    #                         channels: <List>
-    #                     },
-    #                     selections: [
-    #                         {
-    #                             had_success: <bool>,
-    #                             chan_selected: <int>,
-    #                             prob_success: <float>
-    #                         }, ...
-    #                     ]
-    #                 }
-    #             }
-    #         }, ...
-    #     }
-    #     This json string is used for both
-    #     self.retrieve_system.. and self.retrieve_query... . The only difference
-    #     is that the former contains all the values in the session and the 
-    #     latter contains just that particular query.
-    #     """
-    #     def parse_exp(result_list):
-    #         json_dict = dict()
-    #         for result in result_list:
-    #             node_key = result.nodes[0] + "_" + result.nodes[1]
-    #             channel = self._convert_channel_to_dict(result.channel)
-    #             channel_selections = \
-    #                 [
-    #                     {
-    #                         "had_success": chan_result.had_success,
-    #                         "chan_selected": chan_result.channel_selected,
-    #                         "prob_success": chan_result.prob_of_success
-    #                     } for chan_result in result.channel_selection
-    #                 ]
-    #             json_dict[node_key] = {
-    #                 "channel": channel,
-    #                 "selections": channel_selections
-    #             }
-    #         return json_dict
-            
-    #     date_string, stat_block = stat_pkg
-    #     json_dict[date_string] = {
-    #         "cost": round(stat_block.cost,4),
-    #         "path": stat_block.best_route,
-    #         "results": parse_exp(stat_block.exp_results)
-    #     }
-    #     return json_dict
-    
-    # def _convert_channel_to_dict(self, chan):
-    #     return {
-    #         "id": chan.c_id,
-    #         "weight": chan.channel_weight,
-    #         "channels": chan.channel_system
-    #     }
-
 if __name__ == "__main__":
     """
     Run this file as the entry to envoke the CLI version of this application
